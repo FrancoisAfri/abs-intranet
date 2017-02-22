311804b64d5d3c39a11fa6d4deff491bdef99fe4
Smalto Tsham <smalto@afrixcel.co.za>
2017-02-22 14:11:07 +0200

    removed dashboard widgets

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,45 +28,15 @@ class DashboardController extends Controller
         $user = Auth::user()->load('person');
 		
         if ($user->type === 1 || $user->type === 3) {
-            
-            $projects = projects::orderBy('id', 'desc')->limit(10)->get();
-            $activity = activity::orderBy('id', 'desc')->limit(10)->get()->load('project');
-
-            $newprojects = projects::where('status', 1)->count();
-            $newactivities = activity::where('status', 1)->count();
-            $newprogrammes = programme::where('status', 1)->count();
-
-            $canViewProject = (($user->type === 3) || ($user->type === 1 && $user->person->position > 0 && $user->person->position <= 4)) ? true : false;
-            $canView = (($user->type === 3) || ($user->type === 1 && $user->person->position > 0 && $user->person->position <= 3)) ? true : false;
-            
-
-            $projectStatus = ['' => '', -1 => "Rejected", 1 => "Pending Approval", 2 => 'Approved', 3 => 'Completed'];
-            $statusLabels = [-1 => "label-danger", 1 => "label-warning", 2 => 'label-success', 3 => 'label-info'];
 
             $activityStatus = ['' => '', -1 => "Rejected", 1 => "Pending Approval", 2 => 'Approved', 3 => 'Completed'];
             $statusLabels = [-1 => "label-danger", 1 => "label-warning", 2 => 'label-success', 3 => 'label-info'];
 
-
-            $data['projects'] = $projects;
-            $data['status_array'] = $projectStatus;
-
-            $data['activity'] = $activity;
             $data['status_array'] = $activityStatus;
-            $data['canViewProject'] = $canViewProject;
-            $data['canView'] = $canView;
             $data['user'] = $user;
-
-
             $data['statusLabels'] = $statusLabels;
-
-            $data['newprojects'] = $newprojects;
-            $data['newactivities']= $newactivities;
-            $data['newprogrammes']= $newprogrammes;
-
             $data['page_title'] = "Admin Dashboard";
 			$data['page_description'] = "Administrator main Dashboard";
-
-           //return  $newprogrammes;
             
          return view('dashboard.admin_dashboard')->with($data); //Admin Dashboard
         }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,9 @@
     <!-- Logo -->
     <a href="/" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>O</b>ED</span>
+        <span class="logo-mini"><b>A</b>BS</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>OSIZWENI</b> EDC</span>
+        <span class="logo-lg"><b>AFRIXCEL</b> BS</span>
     </a>
 
     <!-- Header Navbar -->
