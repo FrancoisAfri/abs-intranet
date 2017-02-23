2b1ed226fd373d7d280c415a3331d03513f10c72
Smalto Tsham <smalto@afrixcel.co.za>
2017-02-23 17:41:03 +0200

    created the DivivsionLevel model for the division_setup table

---
+++ app/DivisionLevel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DivisionLevel extends Model
+{
+    //Specify the table name
+    public $table = 'division_setup';
+
+    // Mass assignable fields
+    protected $fillable = [
+        'level', 'name', 'active'
+    ];
+}
