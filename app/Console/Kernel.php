190ea5ac7aa77d34e225571c064389d3c6c12d8b
Smalto <smalto@afrixel.co.za>
2017-05-31 11:22:25 +0200

    fixed task error on dashboard

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //\App\Console\Commands\Inspire::class,
         \App\Console\Commands\SendStatement::class,
+        \App\Console\Commands\EmployeeTasksOverdue::class,
     ];
 
     /**
