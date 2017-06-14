c2bf6a1c41e05727f353dd6efa7e85d31f4fbfdb
Smalto <smalto@afrixel.co.za>
2017-06-14 15:59:57 +0200

    Fix Appraisal search error on line 123

--- app/AppraisalKPIResult.php
+++ app/AppraisalKPIResult.php
@@ -120,8 +120,12 @@ class AppraisalKPIResult extends Model
                     $lowestRange = $kpi->kpiranges->where('status', 1)->min('range_from');
                     $highestRange = $kpi->kpiranges->where('status', 1)->max('range_to');
                     //$highestDeduction = $kpi->kpiranges->where('status', 1)->max('lowest');
-                    $highestDeduction = ($kpi && $kpi->kpiranges && $kpi->kpiranges->where('status', 1)) ? $kpi->kpiranges->where('status', 1)->sortByDesc('range_to')->first()->percentage : 0;
-
+                    //$highestDeduction = ($kpi->kpiranges && $kpi->kpiranges->where('status', 1)) ? $kpi->kpiranges->where('status', 1)->sortByDesc('range_to')->first()->percentage : 0;
+                    try {
+                        $highestDeduction = $kpi->kpiranges->where('status', 1)->sortByDesc('range_to')->first()->percentage;
+                    } catch (Exception $e) {
+                        $highestDeduction = 0;
+                    }
                     if ($score < $lowestRange) $percentage = 0;
                     elseif ($score > $highestRange) $percentage = $highestDeduction;
                     else {
