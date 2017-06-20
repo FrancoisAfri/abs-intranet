6af91e0d803fbe974dc798263efb822a6d736775
Francois Keou <francois@afrixcel.co.za>
2017-06-20 17:05:55 +0200

    d

--- app/Http/Controllers/AppraisalKPIResultsController.php
+++ app/Http/Controllers/AppraisalKPIResultsController.php
@@ -217,10 +217,14 @@ class AppraisalKPIResultsController extends Controller
 								if (!empty($employees->jobTitle) && !empty($employees->jobTitle->kpiTemplate) && !empty($employees->jobTitle->kpiTemplate->id) && ($employees->jobTitle->kpiTemplate->id == $kip->template_id))
 								{
 									if ($uploadType == 1)
+									{
+										if ($employeeNO == $employees->id) continue;
 										$insert[] = ['kpi_id' => $kip->id,'template_id' => $kip->template_id,
 										'score' => $value['result'], 
 										'date_uploaded' => $templateData['date_uploaded'],
 										'hr_id' => $employees->id];
+										$employeeNO = $employees->id;
+									}
 									elseif ($uploadType == 2) // Make calculations if clockin time is greater than normal time late else not late
 									{// 1 for late, 2 for not late
 										$attendance = 2;
