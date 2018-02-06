c1129513384ba9c3ec285b6bcc4d7ea6cddf28b2
Francois Keou <francois@afrixcel.co.za>
2018-02-06 14:25:12 +0200

    dd

--- app/unresolved_tickets_settings.php
+++ app/unresolved_tickets_settings.php
@@ -10,9 +10,12 @@ class unresolved_tickets_settings extends Model
 	
 	// Mass assignable fields
     protected $fillable = [
-      'tickets_low',  'low_ah', 'esc_low_email', 'esc_low_sms','aftoffice_hrs_low_email','aftoffice_hrs_low_sms',
-      'tickets_normal','normal_oficehrs', 'office_hrs_normal_email','office_hrs_normal_sms', 'aftoffice_hrs_nomal_email',
-        'aftoffice_hrs_nomal_sms', 'tickets_high', 'high_oficehrs','office_hrs_high_email',  'office_hrs_high_sms', 'aftoffice_hrs_high_email',  'aftoffice_hrs_high_sms',  'auto_critical', 'office_hrs_critical', 'notify_level_critical',
-           'office_hrs_critical_email', 'office_hrs_critical_sms','aftoffice_hrs_critical_email','aftoffice_hrs_critical_sms'
-           ];
+      'tickets_low',  'low_ah', 'esc_low_email', 'esc_low_sms','aftoffice_hrs_low_email'
+	  ,'aftoffice_hrs_low_sms','tickets_normal','normal_oficehrs', 'office_hrs_normal_email'
+	  ,'office_hrs_normal_sms', 'aftoffice_hrs_nomal_email','aftoffice_hrs_nomal_sms',
+	  'tickets_high', 'high_oficehrs','office_hrs_high_email',  'office_hrs_high_sms',
+	  'aftoffice_hrs_high_email',  'aftoffice_hrs_high_sms',  'auto_critical', 
+	  'office_hrs_critical', 'notify_level_critical','office_hrs_critical_email',
+	  'office_hrs_critical_sms','aftoffice_hrs_critical_email','aftoffice_hrs_critical_sms',
+	  'helpdesk_id'];
 }
