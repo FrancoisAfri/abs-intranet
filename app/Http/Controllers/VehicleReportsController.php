9e6c0d1b2b2c3794ab217c66ebceb42d1563ed36
Nkosana Ncube <nkosana@afrixcel.co.za>
2018-03-26 09:16:51 +0200

    knjn

--- app/Http/Controllers/VehicleReportsController.php
+++ app/Http/Controllers/VehicleReportsController.php
@@ -852,12 +852,12 @@ class VehicleReportsController extends Controller
 
 
         $fuelLog = DB::table('vehicle_fuel_log')
-            ->select('vehicle_fuel_log.*', 'vehicle_fuel_log.status as Status','vehicle_fuel_log.id as fuelLogID', 'vehicle_details.vehicle_make as vehiclemake','vehicle_details.vehicle_model as vehiclemodel','vehicle_details.vehicle_type as vehicletype','vehicle_make.name as VehicleMake','vehicle_model.name as VehicleModel',
-             'vehicle_managemnet.name as vehicletypes')
+            ->select('vehicle_fuel_log.*', 'vehicle_fuel_log.status as Status','vehicle_fuel_log.id as fuelLogID', 'vehicle_details.vehicle_make as vehiclemake','vehicle_details.vehicle_model as vehiclemodel','vehicle_details.vehicle_type as vehicletype','vehicle_make.name as VehicleMake','vehicle_model.name as VehicleModel','vehicle_managemnet.name as vehicletypes' , 'hr_people.first_name as firstname', 'hr_people.surname as surname')
             ->leftJoin('vehicle_details', 'vehicle_details.id', '=', 'vehicle_details.id')
             ->leftJoin('vehicle_make', 'vehicle_details.id', '=', 'vehicle_make.id')
             ->leftJoin('vehicle_model', 'vehicle_details.id', '=', 'vehicle_model.id')
             ->leftJoin('vehicle_managemnet', 'vehicle_details.id', '=', 'vehicle_managemnet.id')
+           ->leftJoin('hr_people', 'vehicle_fuel_log.driver', '=', 'hr_people.id')
             ->where(function ($query) use ($vehicleType) {
                     if (!empty($vehicleType)) {
                         $query->where('vehicle_type', $vehicleType);
@@ -881,7 +881,7 @@ class VehicleReportsController extends Controller
                     })
                 ->get();
 
-               return $fuelLog;
+              // return $fuelLog;
 
             
 
