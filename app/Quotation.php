ff5554720771bd135827df4116db41501c1339b5
Francois Keou <francois@afrixcel.co.za>
2018-07-24 11:35:17 +0200

    hh

--- app/Quotation.php
+++ app/Quotation.php
@@ -14,7 +14,7 @@ class Quotation extends Model
         'company_id', 'client_id', 'division_id', 'division_level', 'hr_person_id'
 		, 'approval_person_id', 'status','send_date', 'approval_date', 'discount_percent'
 		, 'add_vat', 'payment_option', 'payment_term','first_payment_date', 'account_id'
-		, 'quote_number', 'quote_type', 'quote_title', 'quote_remarks'
+		, 'quote_number', 'quote_type', 'quote_title', 'quote_remarks', 'quote_date'
     ];
 
     //quotation status
