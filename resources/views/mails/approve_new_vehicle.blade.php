e0d609f1c580873c6c6247a52e14dcd14104c988
Francois Keou <francois@afrixcel.co.za>
2018-09-06 15:54:50 +0200

    df

--- resources/views/mails/approve_new_vehicle.blade.php
+++ resources/views/mails/approve_new_vehicle.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{{ $company_name }} - Quote Approval Required</title>
+    <title>{{ $company_name }} - Vehicle Approval Required</title>
     <style type="text/css" media="screen">
 
         /* Force Hotmail to display emails at full width */
