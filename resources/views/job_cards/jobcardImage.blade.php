78417b3fe42984a958db9e9e4f7d12303be6faca
Francois Keou <francois@afrixcel.co.za>
2018-10-23 14:58:47 +0200

    df

--- resources/views/job_cards/jobcardImage.blade.php
+++ resources/views/job_cards/jobcardImage.blade.php
@@ -106,131 +106,129 @@
                             <button type="button" id="cat_module" class="btn btn-warning pull-right" data-toggle="modal"
                                     data-target="#upload-image-modal">Upload New Image
                             </button>
-                        </div>
-                        
+                        </div>         
                  @include('job_cards.partials.edit_image_modal')
                  @include('job_cards.partials.upload_newImage_modal')
-
                     </div>
                 </div>
             </div>
-            @endsection
-            @section('page_script')
-                <script src="/bower_components/bootstrap_fileinput/js/plugins/sortable.min.js"
-                        type="text/javascript"></script>
-                <!-- the main fileinput plugin file -->
-                <script src="/bower_components/bootstrap_fileinput/js/fileinput.min.js"></script>
-                <!-- optionally if you need a theme like font awesome theme you can include it as mentioned below -->
-                <script src="/bower_components/bootstrap_fileinput/themes/fa/theme.js"></script>
-                <script src="/custom_components/js/modal_ajax_submit.js"></script>
-				<!-- iCheck -->
-				<script src="/bower_components/AdminLTE/plugins/iCheck/icheck.min.js"></script>
-    
-                <script>
-                   $('#back_button').click(function () {
-                            location.href = '/jobcards/viewcard/{{$images->id}}';
-                        });
-                    $(function () {
-						$('.zip-field').hide();
-						$('.single-field').show();
-						//Initialize iCheck/iRadio Elements
-						$('input').iCheck({
-							checkboxClass: 'icheckbox_square-blue',
-							radioClass: 'iradio_square-blue',
-							increaseArea: '20%' // optional
-						});
-                        var moduleId;
-                        //Tooltip
-                        $('[data-toggle="tooltip"]').tooltip();
+        </div>
+    </div>
+@endsection
+@section('page_script')
+	<script src="/bower_components/bootstrap_fileinput/js/plugins/sortable.min.js"
+			type="text/javascript"></script>
+	<!-- the main fileinput plugin file -->
+	<script src="/bower_components/bootstrap_fileinput/js/fileinput.min.js"></script>
+	<!-- optionally if you need a theme like font awesome theme you can include it as mentioned below -->
+	<script src="/bower_components/bootstrap_fileinput/themes/fa/theme.js"></script>
+	<script src="/custom_components/js/modal_ajax_submit.js"></script>
+	<!-- iCheck -->
+	<script src="/bower_components/AdminLTE/plugins/iCheck/icheck.min.js"></script>
 
-                        //Vertically center modals on page
-                        function reposition() {
-                            var modal = $(this),
-                                dialog = modal.find('.modal-dialog');
-                            modal.css('display', 'block');
+	<script>
+	   $('#back_button').click(function () {
+				location.href = '/jobcards/viewcard/{{$images->id}}';
+			});
+		$(function () {
+			$('.zip-field').hide();
+			$('.single-field').show();
+			//Initialize iCheck/iRadio Elements
+			$('input').iCheck({
+				checkboxClass: 'icheckbox_square-blue',
+				radioClass: 'iradio_square-blue',
+				increaseArea: '20%' // optional
+			});
+			var moduleId;
+			//Tooltip
+			$('[data-toggle="tooltip"]').tooltip();
 
-                            // Dividing by two centers the modal exactly, but dividing by three
-                            // or four works better for larger screens.
-                            dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
-                        }
-                        // Reposition when a modal is shown
-                        $('.modal').on('show.bs.modal', reposition);
-                        // Reposition when the window is resized
-                        $(window).on('resize', function () {
-                            $('.modal:visible').each(reposition);
-                        });
-                        //
-                        $(function () {
-                            $('img').on('click', function () {
-                                $('.enlargeImageModalSource').attr('src', $(this).attr('src'));
-                                $('#enlargeImageModal').modal('show');
-                            });
-                        });
+			//Vertically center modals on page
+			function reposition() {
+				var modal = $(this),
+					dialog = modal.find('.modal-dialog');
+				modal.css('display', 'block');
 
-                        $('#rdo_single, #rdo_zip').on('ifChecked', function () {
-							
-                            var allType = hideFields();
-                        });
-                        function hideFields() {
-                            var allType = $("input[name='upload_type']:checked").val();
-                            if (allType == 1) {
-                                $('.zip-field').hide();
-                                $('.single-field').show();
-                            }
-                            else if (allType == 2) {
-                                $('.single-field').hide();
-                                $('.zip-field').show();
-                            }
-                            return allType;
-                        }
+				// Dividing by two centers the modal exactly, but dividing by three
+				// or four works better for larger screens.
+				dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+			}
+			// Reposition when a modal is shown
+			$('.modal').on('show.bs.modal', reposition);
+			// Reposition when the window is resized
+			$(window).on('resize', function () {
+				$('.modal:visible').each(reposition);
+			});
+			//
+			$(function () {
+				$('img').on('click', function () {
+					$('.enlargeImageModalSource').attr('src', $(this).attr('src'));
+					$('#enlargeImageModal').modal('show');
+				});
+			});
 
-                        //Post perk form to server using ajax (add)
-                        $('#add-vehicle_image').on('click', function () {
+			$('#rdo_single, #rdo_zip').on('ifChecked', function () {
+				
+				var allType = hideFields();
+			});
+			function hideFields() {
+				var allType = $("input[name='upload_type']:checked").val();
+				if (allType == 1) {
+					$('.zip-field').hide();
+					$('.single-field').show();
+				}
+				else if (allType == 2) {
+					$('.single-field').hide();
+					$('.zip-field').show();
+				}
+				return allType;
+			}
 
-                            var strUrl = '/jobcard/addimages';
-                            var formName = 'add-new-vehicleImage-form';
-                            var modalID = 'upload-image-modal';
-                            //var modal = $('#'+modalID);
-                            var submitBtnID = 'add-vehicle_image';
-                            var redirectUrl = '/jobcards/jobcardimages/{{$images->id}}';
-                            var successMsgTitle = 'Image Added!';
-                            var successMsg = 'The Image  has been updated successfully.';
-                            modalFormDataSubmit(strUrl, formName, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
-                        });
-                        
-                        
-                        var ImageID;
-                        $('#edit-package-modal').on('show.bs.modal', function (e) {
-                            //console.log('kjhsjs');
-                            var btnEdit = $(e.relatedTarget);
-                              if (parseInt(btnEdit.data('id')) > 0) {
-                              ImageID = btnEdit.data('id');
-                             }
-                            var name = btnEdit.data('name');
-                            var description = btnEdit.data('description');
-                            var valueID = btnEdit.data('valueID');
-                            var jobcard_id = btnEdit.data('jobcard_id');
-                            var images = btnEdit.data('images');
-                            var modal = $(this);
-                            modal.find('#name').val(name);
-                            modal.find('#description').val(description);
-                            modal.find('#jobcard_id').val(jobcard_id);
-                            modal.find('#image').val(images);
-                            modal.find('#valueID').val(valueID);
-                        });
-                        $('#edit_image').on('click', function () {
-                            var strUrl = '/vehicle_management/edit_images/' + ImageID;
-                            var formName = 'edit-image-form';
-                            var modalID = 'edit-package-modal';
-                            var submitBtnID = 'edit_image';
-                            var redirectUrl = '/vehicle_management/viewImage';
-                            var successMsgTitle = 'Image Modified!';
-                            var successMsg = 'The Image  has been updated successfully.';
-                            var Method = 'PATCH'
-                            modalFormDataSubmit(strUrl, formName, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
-                        });
+			//Post perk form to server using ajax (add)
+			$('#add-vehicle_image').on('click', function () {
 
-
-                    });
-                </script>
+				var strUrl = '/jobcard/addimages';
+				var formName = 'add-new-vehicleImage-form';
+				var modalID = 'upload-image-modal';
+				//var modal = $('#'+modalID);
+				var submitBtnID = 'add-vehicle_image';
+				var redirectUrl = '/jobcards/jobcardimages/{{$images->id}}';
+				var successMsgTitle = 'Image Added!';
+				var successMsg = 'The Image  has been updated successfully.';
+				modalFormDataSubmit(strUrl, formName, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
+			});
+			
+			
+			var ImageID;
+			$('#edit-package-modal').on('show.bs.modal', function (e) {
+				//console.log('kjhsjs');
+				var btnEdit = $(e.relatedTarget);
+				  if (parseInt(btnEdit.data('id')) > 0) {
+				  ImageID = btnEdit.data('id');
+				 }
+				var name = btnEdit.data('name');
+				var description = btnEdit.data('description');
+				var valueID = btnEdit.data('valueID');
+				var jobcard_id = btnEdit.data('jobcard_id');
+				var jcImage = btnEdit.data('jc_image');
+				var modal = $(this);
+				modal.find('#name').val(name);
+				modal.find('#description').val(description);
+				modal.find('#jobcard_id').val(jobcard_id);
+				modal.find('#jc_image').attr("src", jcImage);
+				modal.find('#valueID').val(valueID);
+			});
+			$('#edit_image').on('click', function () {
+				var strUrl = '/vehicle_management/edit_images/' + ImageID;
+				var formName = 'edit-image-form';
+				var modalID = 'edit-package-modal';
+				var submitBtnID = 'edit_image';
+				var redirectUrl = '/vehicle_management/viewImage';
+				var successMsgTitle = 'Image Modified!';
+				var successMsg = 'The Image  has been updated successfully.';
+				var Method = 'PATCH'
+				modalFormDataSubmit(strUrl, formName, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
+			});
+		});
+	</script>
 @endsection
