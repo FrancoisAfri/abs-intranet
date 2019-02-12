5f2cc796a3a77ef6d0a7f838f4ecbbeff5c23d60
Francois Keou <francois@afrixcel.co.za>
2019-02-12 19:39:12 +0200

    df

--- app/ProcurementRequest.php
+++ app/ProcurementRequest.php
@@ -14,17 +14,17 @@ class ProcurementRequest extends Model
         'employee_id', 'on_behalf_of', 'on_behalf_employee_id', 'date_created'
 		, 'title_name', 'status',  'date_approved', 'special_instructions'
 		, 'detail_of_expenditure', 'justification_of_expenditure', 'po_number', 'invoice_number'
-		, 'delivery_number', 'request_collected', 'item_type', 'collection_document'
-		, 'collection_note'];
+		, 'delivery_number', 'request_collected', 'item_type', 'jobcard_id'
+		, 'stock_request_id', 'collection_note', 'collection_document'];
 		
 	/**
-     * Relationship between RequestStock and RequestStockItems
+     * Relationship between procurement and ProcurementRequestItems
      *
      * @return \Illuminate\Database\Eloquent\Relations\hasManyTo
      */
-    public function stockItems()
+    public function procurementItems()
     {
-        return $this->hasMany(RequestStockItems::class, 'request_stocks_id');
+        return $this->hasMany(ProcurementRequestItems::class, 'procurement_id');
     }
 	public function employees()
     {
@@ -33,5 +33,10 @@ class ProcurementRequest extends Model
 	public function employeeOnBehalf()
     {
         return $this->belongsTo(HRPerson::class, 'on_behalf_employee_id');
+    }
+	
+	public function requestStatus()
+    {
+        return $this->belongsTo(ProcurementApproval_steps::class, 'status');
     }
 }
