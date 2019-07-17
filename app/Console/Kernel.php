84544439a5e8a8c63a51591ab011f82c95d57821
Francois Keou <francois@afrixcel.co.za>
2019-07-17 12:09:17 +0200

    sds

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //\App\Console\Commands\Inspire::class,
-        //\App\Console\Commands\EmployeeTasksOverdue::class,
+        \App\Console\Commands\EmployeeTasksOverdue::class,
          \App\Console\Commands\leavemanagementAnnual::class,
 		  \App\Console\Commands\leavemanagementFamily::class,
 		  \App\Console\Commands\LeaveManagementSickLeave::class,
@@ -35,19 +35,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //$filePath = '/storage/logs/tasks.log';
+		
+        $filePath = '/storage/logs/tasks.log';
         //$schedule->command('send:statement')->everyMinute()->appendOutputTo($filePath);
-        $schedule->command('emptask:overdue')->daily();
-        $schedule->command('schedule:leaveAllocationAnnual')->monthlyOn(1, '01:00');
-		$schedule->command('schedule:leaveAllocationFamily')->dailyAt('02:00');
-		$schedule->command('schedule:leaveAllocationSick')->dailyAt('03:00');
-		$schedule->command('schedule:leaveAllocationPartenity')->dailyAt('04:00');
-		$schedule->command('schedule:leaveResetSick')->dailyAt('06:00');
-		$schedule->command('schedule:leaveResetPartenity')->cron('0 0 1 1 * *');
-		$schedule->command('schedule:leaveResetFamily')->cron('0 1 1 1 * *');
-		$schedule->command('fleet:documentsUpload')->everyTenMinutes();
-		$schedule->command('vehicle:variouDocumentsUpload')->everyThirtyMinutes();
-		//$schedule->command('fleet:documentsUpload')->cron('0 1 1 1 * *');
+        $schedule->command('emptask:overdue')->daily()->appendOutputTo($filePath);
+        $schedule->command('schedule:leaveAllocationAnnual')->monthlyOn(1, '01:00')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveAllocationFamily')->dailyAt('02:00')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveAllocationSick')->dailyAt('03:00')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveAllocationPartenity')->dailyAt('04:00')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveResetSick')->dailyAt('06:00')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveResetPartenity')->cron('0 0 1 1 * *')->appendOutputTo($filePath);
+		$schedule->command('schedule:leaveResetFamily')->cron('0 1 1 1 * *')->appendOutputTo($filePath);
+		$schedule->command('fleet:documentsUpload')->everyTenMinutes()->appendOutputTo($filePath);
+		$schedule->command('vehicle:variouDocumentsUpload')->everyThirtyMinutes()->appendOutputTo($filePath);
     }
 
     /**
