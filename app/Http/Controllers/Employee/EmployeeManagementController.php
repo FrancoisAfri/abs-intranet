f67aed1184d6ea61beed1d6ba2ddcf2bd601c3c1
Nkosana <ncubesss@gmail.com>
2022-10-17 07:17:47 +0200

    removed testing data

--- app/Http/Controllers/Employee/EmployeeManagementController.php
+++ app/Http/Controllers/Employee/EmployeeManagementController.php
@@ -139,7 +139,9 @@ class EmployeeManagementController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        // get user details
+        /**
+         * get user location
+         */
         $latLong = $request['latitudes'] . ',' . $request['longitudes'];
 
         $location = $this->getLocation($latLong);
