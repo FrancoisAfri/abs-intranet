06fc048a9d408db5763fdec2d9885c7c8231b8f8
FrancoisAfri <francois@afrixcel.co.za>
2022-11-10 13:23:40 +0200

    Update create-asset-type.blade.php

--- resources/views/assets/assetType/create-asset-type.blade.php
+++ resources/views/assets/assetType/create-asset-type.blade.php
@@ -16,66 +16,72 @@
                 </div>
                 <div class="box-body">
                     <div style="overflow-X:auto;">
-                        <table id=" " class="display table table-bordered table-hover">
+                            <table id=" " class="display table table-bordered table-hover">
                             <thead>
-								<tr>
-									<th style="width: 10px; text-align: center;">#</th>
-									<th>Name</th>
-									<th>Description</th>
-									<th style="width: 5px; text-align: center;">.</th>
-									<th style="width: 5px; text-align: center;">.</th>
-								</tr>
+                            <tr>
+                                <th style="width: 10px; text-align: center;">#</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th style="width: 5px; text-align: center;">.</th>
+                                <th style="width: 5px; text-align: center;">.</th>
+                                {{--                                <th style="width: 5px; text-align: center;">.</th>--}}
+                            </tr>
                             </thead>
                             <tbody>
-								@if (count($assetType) > 0)
-									@foreach ($assetType as $key => $assetTypes)
-										<tr id="categories-list">
-											<td nowrap>
-												<button vehice="button" id="edit_asset" class="btn btn-warning  btn-xs"
-														data-toggle="modal" data-target="#edit-asset-modal"
-														data-id="{{ $assetTypes->id }}"
-														data-name="{{ $assetTypes->name }}"
-														data-description="{{$assetTypes->description}}"><i
-															class="fa fa-pencil-square-o"></i> Edit
-												</button>
-											</td>
-											<td>{{ $assetTypes->name ?? ''}} </td>
-											<td>{{ $assetTypes->description ?? ''}} </td>
-											<td>
-												<!--   leave here  -->
-												<button vehice="button" id="view_ribbons" class="btn {{ (!empty($assetTypes->status) && $assetTypes->status == 1) ? " btn-danger " : "btn-success " }}
-									  btn-xs" onclick="postData({{$assetTypes->id}}, 'actdeac');"><i class="fa {{ (!empty($assetTypes->status) && $assetTypes->status == 1) ?
-									  " fa-times " : "fa-check " }}"></i> {{(!empty($assetTypes->status) && $assetTypes->status == 1) ? "De-Activate" : "Activate"}}
-												</button>
-											</td>
-											<td>
-												<form action="{{ route('type.destroy', $assetTypes->id) }}"
-													  method="POST"
-													  style="display: inline-block;">
-													<input type="hidden" name="_method" value="DELETE">
-													<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            @if (count($assetType) > 0)
+                                <ul class="products-list product-list-in-box">
+                                    @foreach ($assetType as $key => $assetTypes)
+                                        <tr id="categories-list">
+                                            <td nowrap>
+                                                <button vehice="button" id="edit_asset" class="btn btn-warning  btn-xs"
+                                                        data-toggle="modal" data-target="#edit-asset-modal"
+                                                        data-id="{{ $assetTypes->id }}"
+                                                        data-name="{{ $assetTypes->name }}"
+                                                        data-description="{{$assetTypes->description}}"><i
+                                                            class="fa fa-pencil-square-o"></i> Edit
+                                                </button>
+                                            </td>
+                                            <td>{{ $assetTypes->name ?? ''}} </td>
+                                            <td>{{ $assetTypes->description ?? ''}} </td>
+                                            <td>
+                                                <!--   leave here  -->
+                                                <button vehice="button" id="view_ribbons" class="btn {{ (!empty($assetTypes->status) && $assetTypes->status == 1) ? " btn-danger " : "btn-success " }}
+                                      btn-xs" onclick="postData({{$assetTypes->id}}, 'actdeac');"><i class="fa {{ (!empty($assetTypes->status) && $assetTypes->status == 1) ?
+                                      " fa-times " : "fa-check " }}"></i> {{(!empty($assetTypes->status) && $assetTypes->status == 1) ? "De-Activate" : "Activate"}}
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('type.destroy', $assetTypes->id) }}"
+                                                      method="POST"
+                                                      style="display: inline-block;">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-													<button type="submit"
-															class="btn btn-xs btn-danger btn-flat delete_confirm"
-															data-toggle="tooltip" title='Delete'>
-														<i class="fa fa-trash"> Delete </i>
+                                                    <button type="submit"
+                                                            class="btn btn-xs btn-danger btn-flat delete_confirm"
+                                                            data-toggle="tooltip" title='Delete'>
+                                                        <i class="fa fa-trash"> Delete </i>
 
-													</button>
-												</form>
-											</td>
-										</tr>
-									@endforeach
-								@endif
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                            @endif
                             </tbody>
                             <tfoot>
-								<tr>
-									<th style="width: 10px; text-align: center;"></th>
-									<th>Name</th>
-									<th>Description</th>
-									<th style="width: 5px; text-align: center;"></th>
-								</tr>
+                            <tr>
+                                <th style="width: 10px; text-align: center;"></th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th style="width: 5px; text-align: center;"></th>
+                                <th style="width: 5px; text-align: center;"></th>
+                                {{--                                <th style="width: 5px; text-align: center;"></th>--}}
+                            </tr>
                             </tfoot>
                         </table>
+
                         <div class="box-footer">
                             <button type="button" id="cat_module" class="btn btn-default pull-right" data-toggle="modal" data-target="#add-asset-modal">Add Asset Type</button>
                             <button type="button" class="btn btn-default pull-left" id="back_button"><i class="fa fa-arrow-left"></i> Back</button>
@@ -86,21 +92,7 @@
                 @include('assets.assetType.partials.create')
                 @include('assets.assetType.partials.edit')
             </div>
-        </div>
-    </div>
-@endsection
-@section('page_script')
-	<!-- DataTables -->
-	<script src="/bower_components/AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
-	<script src="/bower_components/AdminLTE/plugins/datatables/dataTables.bootstrap.min.js"></script>
-	<script src="/custom_components/js/modal_ajax_submit.js"></script>
-	<script src="/custom_components/js/deleteAlert.js"></script>
-	<script src="/custom_components/js/dataTable.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
-	{{--      data tables add ons         --}}
-
-
-	<script type="text/javascript">
+            @endsection
 
             @section('page_script')
                 <!-- DataTables -->
@@ -109,100 +101,104 @@
                 <script src="/custom_components/js/modal_ajax_submit.js"></script>
                 <script src="/custom_components/js/deleteAlert.js"></script>
                 <script src="/custom_components/js/dataTable.js"></script>
-                <script src="{{ asset('custom_components/js/deleteModal.js') }}"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
                 {{--      data tables add ons         --}}
 
 
-		function postData(id , data ){
-			if(data === 'actdeac') location.href = "{{route('type.activate', '')}}"+"/"+id;
-		}
+                <script type="text/javascript">
 
-		$('#back_button').click(function () {
-			location.href = '{{route('assets.settings')}}';
-		});
+                    function postData(id , data ){
+                        if(data === 'actdeac') location.href = "{{route('type.activate', '')}}"+"/"+id;
+                    }
 
-		$('.delete_confirm').click(function (event) {
+                    $('#back_button').click(function () {
+                        location.href = '{{route('assets.settings')}}';
+                    });
 
-			var form = $(this).closest("form");
+                    $('.delete_confirm').click(function (event) {
 
-			var name = $(this).data("name");
+                        var form = $(this).closest("form");
 
-			event.preventDefault();
+                        var name = $(this).data("name");
 
-			swal({
+                        event.preventDefault();
 
-				title: `Are you sure you want to delete this record?`,
-				text: "If you delete this, it will be gone forever.",
-				icon: "warning",
-				buttons: true,
-				dangerMode: true,
-			})
-				.then((willDelete) => {
-					if (willDelete) {
-						form.submit();
-						swal("Poof! Your Record has been deleted!", {
-							icon: "success",
-						});
-					}
+                        swal({
 
-				});
-		});
+                            title: `Are you sure you want to delete this record?`,
+                            text: "If you delete this, it will be gone forever.",
+                            icon: "warning",
+                            buttons: true,
+                            dangerMode: true,
+                        })
+                            .then((willDelete) => {
+                                if (willDelete) {
+                                    form.submit();
+                                    swal("Poof! Your Record has been deleted!", {
+                                        icon: "success",
+                                    });
+                                }
 
-	</script>
+                            });
 
-	<script>
-		$(function () {
-			//Initialize Select2 Elements
+                    });
 
-			 $('#add-asset').on('click', function() {
+                </script>
 
-				let strUrl = '/assets/type';
-				let modalID = 'add-asset-modal';
-				let objData = {
-					name: $('#'+modalID).find('#name').val(),
-					description: $('#'+modalID).find('#description').val(),
-					_token: $('#'+modalID).find('input[name=_token]').val()
-				};
+                <script>
 
-				//console.log(objData)
+                    $(function () {
+                        //Initialize Select2 Elements
 
-				let submitBtnID = 'add-asset';
-				let redirectUrl = '{{ route('type.index') }}';
-				let successMsgTitle = 'Asset Type Added!';
-				let successMsg = 'The Asset Type has been updated successfully.';
-				modalAjaxSubmit(strUrl, objData, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
-			});
+                         $('#add-asset').on('click', function() {
 
-			// show modal info
-			let assetId;
-			$('#edit-asset-modal').on('show.bs.modal', function (e) {
-				let btnEdit = $(e.relatedTarget);
-				assetId = btnEdit.data('id');
-				let name = btnEdit.data('name');
-				let description = btnEdit.data('description');
-				let modal = $(this);
-				modal.find('#name').val(name);
-				modal.find('#description').val(description);
-			});
+                            let strUrl = '/assets/type';
+                            let modalID = 'add-asset-modal';
+                            let objData = {
+                                name: $('#'+modalID).find('#name').val(),
+                                description: $('#'+modalID).find('#description').val(),
+                                _token: $('#'+modalID).find('input[name=_token]').val()
+                            };
 
-			// update modal
-			$('#edit-storeroom').on('click', function () {
+                            console.log(objData)
 
-				let strUrl = '/assets/type/' + assetId;
-				let modalID = 'edit-asset-modal';
-				let objData = {
-					name: $('#'+modalID).find('#name').val(),
-					description: $('#'+modalID).find('#description').val(),
-					_token: $('#'+modalID).find('input[name=_token]').val()
-				};
-				let submitBtnID = 'edit-storeroom';
-				let redirectUrl = '{{route('type.index')}}';
-				let successMsgTitle = 'Changes Saved!';
-				let successMsg = 'Record has been updated successfully.';
-				let Method = 'PATCH';
-				modalAjaxSubmit(strUrl, objData, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg, Method);
-			});
-		});
-	</script>
+                            let submitBtnID = 'add-asset';
+                            let redirectUrl = '{{ route('type.index') }}';
+                            let successMsgTitle = 'Asset Type Added!';
+                            let successMsg = 'The Asset Type has been updated successfully.';
+                            modalAjaxSubmit(strUrl, objData, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg);
+                        });
+
+                        // show modal info
+                        let assetId;
+                        $('#edit-asset-modal').on('show.bs.modal', function (e) {
+                            let btnEdit = $(e.relatedTarget);
+                            assetId = btnEdit.data('id');
+                            let name = btnEdit.data('name');
+                            let description = btnEdit.data('description');
+                            let modal = $(this);
+                            modal.find('#name').val(name);
+                            modal.find('#description').val(description);
+                        });
+
+                        // update modal
+                        $('#edit-storeroom').on('click', function () {
+
+                            let strUrl = '/assets/type/' + assetId;
+                            let modalID = 'edit-asset-modal';
+                            let objData = {
+                                name: $('#'+modalID).find('#name').val(),
+                                description: $('#'+modalID).find('#description').val(),
+                                _token: $('#'+modalID).find('input[name=_token]').val()
+                            };
+                            let submitBtnID = 'edit-storeroom';
+                            let redirectUrl = '{{route('type.index')}}';
+                            let successMsgTitle = 'Changes Saved!';
+                            let successMsg = 'Record has been updated successfully.';
+                            let Method = 'PATCH';
+                            modalAjaxSubmit(strUrl, objData, modalID, submitBtnID, redirectUrl, successMsgTitle, successMsg, Method);
+                        });
+
+                    });
+                </script>
 @endsection
