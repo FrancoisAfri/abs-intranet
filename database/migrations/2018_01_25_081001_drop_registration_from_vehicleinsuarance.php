38d96f30c7c8f8726535ec1ff89da3c4dee8c9a0
FrancoisAfri <francois@afrixcel.co.za>
2024-07-15 14:40:25 +0200

    Update 2018_01_25_081001_drop_registration_from_vehicleinsuarance.php

--- database/migrations/2018_01_25_081001_drop_registration_from_vehicleinsuarance.php
+++ database/migrations/2018_01_25_081001_drop_registration_from_vehicleinsuarance.php
@@ -25,6 +25,8 @@ class DropRegistrationFromVehicleinsuarance extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('vehicle_insurance', function($table) {
+            $table->dropColumn(['registration']);
+         });
     }
 }
